3dca5a919f601d0a8aa96073cce19ebb708b3a69
Фадеева Оля <63449280+olgavf@users.noreply.github.com>
2020-05-07 13:58:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,47 +3,47 @@ using namespace std;
 
 #include <cassert>
 
-template<typename T>
+template<typename N>
 class TPQueue
 {
 private:
-    T *arr;          // массив с данными
+    N *arr;          // массив с данными
     int size;        // максимальное количество элементов в очереди (размер массива)
-    int begin,       // начало очереди
-        end;         // конец очереди
-    int count;       // счетчик элементов
+    int begin;       // начало очереди
+    int end;         // конец очереди
+    int countt;       // счетчик элементов
 public:
     TPQueue(int =100);          // конструктор по умолчанию
     ~TPQueue();                 // деструктор
 
-    void push(const T &); // добавить элемент в очередь
-    T pop();              // удалить элемент из очереди
-    T get() const;        // прочитать первый элемент
+    void push(const N &); // добавить элемент в очередь
+    N pop();              // удалить элемент из очереди
+    N get() const;        // прочитать первый элемент
     bool isEmpty() const;      // пустая ли очередь?
     bool isFull() const;       // заполнен ли массив?
 };
 
 // конструктор по умолчанию
-template<typename T>
-TPQueue<T>::TPQueue(int sizeQueue) :
+template<typename N>
+TPQueue<N>::TPQueue(int sizeQueue) :
     size(sizeQueue), 
-    begin(0), end(0), count(0)
+    begin(0), end(0), countt(0)
 {
     // дополнительный элемент поможет нам различать конец и начало очереди
-    arr = new T[size + 1];
+    arr = new N[size + 1];
 }
 
 // деструктор класса Queue
-template<typename T>
-TPQueue<T>::~TPQueue()
+template<typename N>
+TPQueue<N>::~TPQueue()
 {
     delete [] arr;
 }
 
 
 // функция добавления элемента в очередь
-template<typename T>
-void TPQueue<T>::push(const T & item)
+template<typename N>
+void TPQueue<N>::push(const N & item)
 {
     // проверяем, ести ли свободное место в очереди
     assert( count < size );
@@ -54,14 +54,14 @@ void TPQueue<T>::push(const T & item)
     {
         if (arr[i].prior > arr[i-1].prior)
         {
-            T tmp = arr[i-1];
+            N tmp = arr[i-1];
             arr[i-1] = arr[i];
             arr[i] = tmp;
         }
     }
 
 
-    count++;
+    countt++;
     end++;
 
     // проверка кругового заполнения очереди
@@ -70,14 +70,14 @@ void TPQueue<T>::push(const T & item)
 }
 
 // функция удаления элемента из очереди
-template<typename T>
-T TPQueue<T>::pop()
+template<typename N>
+N TPQueue<N>::pop()
 {
     // проверяем, есть ли в очереди элементы
-    assert( count > 0 );
+    assert( countt > 0 );
 
-    T item = arr[begin++];
-    count--;
+    N item = arr[begin++];
+    countt--;
 
     // проверка кругового заполнения очереди
     if (begin > size)
@@ -87,26 +87,26 @@ T TPQueue<T>::pop()
 }
 
 // функция чтения элемента на первой позиции
-template<typename T>
-T TPQueue<T>::get() const 
+template<typename N>
+N TPQueue<N>::get() const 
 {
     // проверяем, есть ли в очереди элементы
-    assert( count > 0 );
+    assert( countt > 0 );
     return arr[begin];
 }
 
 // функция проверки очереди на пустоту
-template<typename T>
-bool TPQueue<T>::isEmpty() const
+template<typename N>
+bool TPQueue<N>::isEmpty() const
 {
-  return count==0;
+  return countt==0;
 }
 
 // функция проверки очереди на заполненность
-template<typename T>
-bool TPQueue<T>::isFull() const
+template<typename N>
+bool TPQueue<N>::isFull() const
 {
-  return count==size;
+  return countt==size;
 }
 
 struct SYM
